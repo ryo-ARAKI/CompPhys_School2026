094e1f8098f7ffa3d01a9faf5dea7a3005c6093d
ryo-ARAKI <araki.ryo@rs.tus.ac.jp>
2026-03-11 10:14:20 +0900

    implement test for `tb.jl`

--- test/test_tb.jl
+++ test/test_tb.jl
@@ -1,24 +1,26 @@
 using LinearAlgebra
 using Test
+using GrapheneHHG
 
 @testset "TB validity" begin
-    tb0 = TBParams(t=1.0, Δ=0.0)
-    tbΔ = TBParams(t=1.0, Δ=0.2)
+    tb0 = TBParams(; t=1.0, Δ=0.0)
+    tbΔ = TBParams(; t=1.0, Δ=0.2)
 
     for k in kgrid_rhombus(6)
         Hk = H(k, tbΔ)
-        # ここを実装
-        throw(ErrorException("TODO(student): add Hermitian test in test/test_tb.jl"))
+        # Hermite対称性を検証
+        @test Hk ≈ Hk'
     end
 
     for k in kgrid_rhombus(6)
         vals = eigvals(Hermitian(Matrix(H(k, tb0))))
-        # ここを実装
-        throw(ErrorException("TODO(student): add ±E symmetry test in test/test_tb.jl"))
+        # 二つの固有値が逆符号であることを検証
+        @test vals[1] ≈ -vals[2]
     end
 
     K = GrapheneHHG.Vec2(2 * pi / (3 * sqrt(3)), 2 * pi / 3)
     valsK = eigvals(Hermitian(Matrix(H(K, tb0))))
-    # ここを実装
-    throw(ErrorException("TODO(student): add K-point gapless test in test/test_tb.jl"))
+    # 固有値が二つとも0であることを検証
+    @test valsK[1] ≈ 0.0 atol=1e-12
+    @test valsK[2] ≈ 0.0 atol=1e-12
 end
